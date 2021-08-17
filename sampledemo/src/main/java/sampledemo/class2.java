63bf33d60cae7341345d9f1039151ce8cc787e89
KIRTI CHOUDHARY <KIRTI CHOUDHARY@LAPTOP-KIRTI>
2021-08-17 09:23:10 +0530

    Loginuser class added.

--- sampledemo/src/main/java/sampledemo/class2.java
+++ sampledemo/src/main/java/sampledemo/class2.java
@@ -1,5 +1,18 @@
 package sampledemo;
 
 public class class2 {
+	
+	public void display() {
+		int[] arr= {23,45,78};
+		for(int num:arr)
+			System.out.print(num);
+	}
+	
+	public void print() {
+		System.out.println("Print all array numbers:");
+		display();
+			
+		}
+	}
 
-}
+
