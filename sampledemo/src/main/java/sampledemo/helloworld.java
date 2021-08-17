d6c14324f1192c1177fe3a6bd3608cd4fa293a16
KIRTI CHOUDHARY <KIRTI CHOUDHARY@LAPTOP-KIRTI>
2021-08-17 10:50:56 +0530

    commit

--- sampledemo/src/main/java/sampledemo/helloworld.java
+++ sampledemo/src/main/java/sampledemo/helloworld.java
@@ -3,14 +3,13 @@ package sampledemo;
 public class helloworld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World. have a nice day");
 		System.out.println(" have a nice day");
 
-=======
 		System.out.println("Hello World. Have a nice day.");
 		System.out.println("Hello World. Have a nice day.");
->>>>>>> refs/heads/login-branch
+		System.out.println("Hello World. Have a nice day.");
+		System.out.println("Hello World. Have a nice day.");
 
 	}
 
