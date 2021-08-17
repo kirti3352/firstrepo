c97c67eae5023c7bc95bd2b22b79f407e2bd7b60
KIRTI CHOUDHARY <KIRTI CHOUDHARY@LAPTOP-KIRTI>
2021-08-17 10:41:39 +0530

    commiy

--- sampledemo/src/main/java/sampledemo/helloworld.java
+++ sampledemo/src/main/java/sampledemo/helloworld.java
@@ -3,7 +3,9 @@ package sampledemo;
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		System.out.println("Hello World. have a nice day");
+		System.out.println(" have a nice day");
+
 
 	}
 
